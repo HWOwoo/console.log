13185ef4543727e079d49ec28b91bf2ca983a97a
HWOwoo <nelap1234@naver.com>
2022-03-19 12:02:13 +0900

    console

---
+++ js/web.js
@@ -0,0 +1,20 @@
+/* var count = "count 라는 문자저장 공간을 확보";
+count += "수정본"; 덧붙여서 저장 */
+
+var count = 50;
+count += 50;
+count += "원";
+
+count += 10;
+count = 100;
+
+var count = "100원"; 
+var arr = ["회사소개","제품소개","고객센터"];
+
+
+// number, string, null, undefined, Object 
+
+
+console.log("count의 변수값은 ", count, "데이터 타입은", typeof count);
+console.log("arr의 변수값은 ", arr[0], "데이터 타입은", typeof arr);
+// count 변수값은 ? ... 이고, go 값은 고릴라
